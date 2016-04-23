bca2f33702c1e4e29a02fa48934f987e2cc81a3e
higerra <higerra@126.com>
2016-04-22 23:23:29 -0500

    Hang

--- code/tracking.cpp
+++ code/tracking.cpp
@@ -5,5 +5,10 @@
 #include "tracking.h"
 
 namespace substab{
+	namespace Tracking{
+		void genTrackMatrix(const std::vector<cv::Mat>& images, Eigen::MatrixXd& trackMatrix){
+			int N = 0;
 
+		}
+	}
 }//namespace substab
--- code/tracking.h
+++ code/tracking.h
@@ -5,9 +5,14 @@
 #ifndef SUBSPACESTAB_TRACKING_H
 #define SUBSPACESTAB_TRACKING_H
 
-namespace substab {
-    class Tracking {
+#include <opencv2/opencv.hpp>
+#include <Eigen/Eigen>
+#include <vector>
+#include <string>
 
-    };
+namespace substab {
+	namespace Tracking {
+		void genTrackMatrix(const std::vector<cv::Mat>& images, Eigen::MatrixXd& trackMatrix);
+	}
 }
 #endif //SUBSPACESTAB_TRACKING_H
