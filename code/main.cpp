808e6dbc15e1e25e56d2fedfe329a9ffd9e3ae21
higerra <higerra@126.com>
2016-04-25 23:52:49 -0500

    Hang

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <gflags/gflags.h>
+#include <opencv2/opencv.hpp>
+#include <string>
+
 using namespace std;
 
+void importVideo(const std::string& path, std::vector<cv::Mat>& images);
+
 int main(int argc, char** argv){
+	if(argc < 2){
+		cerr << "Usage ./SubspaceStab <path-to-data>" << endl;
+		return 1;
+	}
+
+
+
     return 0;
+}
+
+void importVideo(const std::string& path, std::vector<cv::Mat>& images){
+	
 }
