f80304a1f6dfae8f11ca8bbad9891d450c348e13
higerra <higerra@126.com>
2016-04-26 00:50:08 -0500

    Hang

--- code/main.cpp
+++ code/main.cpp
@@ -2,8 +2,11 @@
 #include <gflags/gflags.h>
 #include <opencv2/opencv.hpp>
 #include <string>
+#include "tracking.h"
+#include "warping.h"
 
 using namespace std;
+using namespace cv;
 
 void importVideo(const std::string& path, std::vector<cv::Mat>& images);
 
@@ -13,11 +16,25 @@ int main(int argc, char** argv){
 		return 1;
 	}
 
+	vector<Mat> images;
+	importVideo(string(argv[1]), images);
 
+	printf("%d frames read\n", (int)images.size());
+	for(auto i=0; i<images.size(); ++i){
+		imshow("a", images[i]);
+		waitKey();
+	}
 
     return 0;
 }
 
 void importVideo(const std::string& path, std::vector<cv::Mat>& images){
-	
+	VideoCapture cap(path);
+	CHECK(cap.isOpened()) << "Can not open video " << path;
+	Mat frame;
+	while(true){
+		if(!cap.read(frame))
+			break;
+		images.push_back(frame);
+	}
 }
