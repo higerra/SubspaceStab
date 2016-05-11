aa20b59bd60e63e45fba01f5502a8ace350feaa3
higerra <higerra@126.com>
2016-05-11 10:28:36 -0500

    Hang

---
+++ code/run_all.py
@@ -0,0 +1,9 @@
+import subprocess
+
+datasets = ['SANY0025']
+
+for data in datasets:
+    print('Running '+data+'...')
+    subprocess.check_call(['./build/SubspaceStab', '../data/{}.mp4'.format(data), '--output=build/result_{}'.format(data), '--resize=false'])
+    subprocess.check_call(['ffmpeg', '-i', 'build/result_{}%05d.jpg'.format(data), '-vcodec', 'h264', '-qp', '0', 'result_{}.mp4'.format(data)])
+    subprocess.call('rm build/*.jpg', shell=True)
